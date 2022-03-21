503741e0431e3888b35e9f1d8617e8957353970a
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-03-21 15:51:45 +0300

    add a function to obtain the user input from the DOM

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-    
+    $("#submit-order").click(function() {
+        let sizeOfPizza = $("#select-size option:selected").val();
+        let toppingsOfPizza = $("#select-topping option:selected").val();
+        let crustOfPizza = $("#select-crust option:selected").val();
+        let total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+        let order = 1;
+        let grandTotal = 0;
 
 
 
