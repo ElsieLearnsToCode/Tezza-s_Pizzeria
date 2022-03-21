a081ad8b54e039629dbb1c761d7f8d6d6708fee3
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-03-21 15:59:46 +0300

    create a pizza constructor, to act as a blueprint for every order that a customer makes

--- JS/script.js
+++ JS/script.js
@@ -14,7 +14,13 @@ $(document).ready(function() {
         $("#selected-crust").html($("#select-crust option:selected").text() + " - " + crustOfPizza);
         $("#orderTotal").html(total);
 
-        
+        function Pizza(size, toppings, crust, total, orderNo) {
+            this.size = size;
+            this.toppings = toppings;
+            this.crust = crust;
+            this.total = total;
+            this.orderNo = orderNo;
+          }
     });
 
 
